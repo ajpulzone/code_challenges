2a5b7bf2bcda34c886b650c16d209d5cb79aa601
Annie Pulzone <ajpulzone@gmail.com>
2023-06-30 21:28:06 -0700

    Created method to obtain first element in an array

---
+++ first_element_in_array.rb
@@ -0,0 +1,9 @@
+#Create a function that takes an array containing only numbers and return the first element.
+
+#The first element in an array always has an index of 0.
+
+def get_first_element(array)
+  array.first
+end
+
+p get_first_element([3,9,123])
