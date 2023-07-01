f76d3026f58704682d9f594cb6ecac0b3d960826
Annie Pulzone <ajpulzone@gmail.com>
2023-06-30 21:09:24 -0700

    Created function to return power based on voltage and current

---
+++ power_calculator.rb
@@ -0,0 +1,9 @@
+#Create a function that takes voltage and current and returns the calculated power.
+
+#electric power = voltage difference x current
+
+def power(voltage, current)
+  voltage * current
+end
+
+p power(60, 10)
