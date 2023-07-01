a710d65c762c42da519ecfe769f5fe2473c0546f
Annie Pulzone <ajpulzone@gmail.com>
2023-06-30 20:19:58 -0700

    Created next number method

---
+++ next_number.rb
@@ -0,0 +1,10 @@
+#Return the next number from the integer passed
+#Create a function that takes a number as an argument, increments the number by +1 and returns the result
+
+number = -4
+
+def next_number(number)
+  number + 1
+end
+
+p next_number(number)
