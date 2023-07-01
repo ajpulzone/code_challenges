6ed870efbfe2b18374206a6a6caff44b54ca5142
Annie Pulzone <ajpulzone@gmail.com>
2023-06-30 20:33:20 -0700

    Created function to add the sum of 2 numbers

---
+++ sum_of_two_numbers.rb
@@ -0,0 +1,10 @@
+#Create a function that takes two numbers as arguments and returns their sum
+#Don't forget to return the result.
+#If you get stuck on a challenge, find help in the Resources tab.
+#If you're really stuck, unlock solutions in the Solutions tab.
+
+def sum(num1, num2)
+  num1 + num2
+end
+
+p sum(-4, 12)
