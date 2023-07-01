997971904d7d217c5d8790575277ebe7ebd9f4b7
Annie Pulzone <ajpulzone@gmail.com>
2023-06-30 20:53:27 -0700

    Created method to return the perimeter of a rectangle

---
+++ rectangle_perimeter.rb
@@ -0,0 +1,11 @@
+#Create a function that takes length and width and finds the perimeter of a rectangle.
+
+#Don't forget to return the result.
+#If you're stuck, find help in the Resources tab.
+#If you're really stuck, find solutions in the Solutions tab.
+
+def rectangle_perimeter(length, width)
+  (length * 2) + (width * 2)
+end
+
+p rectangle_perimeter(4, 9)
