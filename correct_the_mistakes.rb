053c4f2e583ef1345a73486355537501b1739b74
Annie Pulzone <ajpulzone@gmail.com>
2023-07-02 22:08:25 -0700

    Corrected mistakes in function

---
+++ correct_the_mistakes.rb
@@ -0,0 +1,12 @@
+#Fix the code in the code tab to pass this challenge (only syntax errors). 
+#Look at the examples below to get an idea of what the function should do.
+
+#Ex: def squaed(b)
+	#return a * a
+#end
+
+def squared(b)
+  return b * b
+end
+
+p squared(3)
