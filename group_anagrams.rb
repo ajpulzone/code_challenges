fa481647e80bbcf55d652c5c9f558951c535b3d6
Annie Pulzone <ajpulzone@gmail.com>
2023-07-03 23:27:17 -0700

    Added new code challende

---
+++ group_anagrams.rb
@@ -0,0 +1,10 @@
+#Write a method to sort an array of strings so that all the anagrams are mext to each other.
+
+def group_anagrams(array)
+  # anagram_array = []
+  # not_anagram_array = []
+
+  # array.each do |string|
+  #   if string == string.reverse
+  # end
+end
