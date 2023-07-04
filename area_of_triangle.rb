39547b0c3cf47b9a72055d8dcb0dd1c98b2382d7
Annie Pulzone <ajpulzone@gmail.com>
2023-07-03 22:20:41 -0700

    Created function to return area of a triangle

---
+++ area_of_triangle.rb
@@ -0,0 +1,9 @@
+#Write a function that takes the base and height of a triangle and return its area.
+
+#The area of a triangle is: (base * height) / 2
+
+def triangle_area(base, height)
+  (base * height)/2
+end
+
+p triangle_area(12, 12)
